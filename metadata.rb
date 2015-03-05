e0f2d73e2f634edd5e893cca224fa9840573f85a
Dan Couture <MathYourLife@gmail.com>
2015-03-04 22:00:17 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'chef-workstation'
+maintainer       'Dan Couture'
+maintainer_email 'mathyourlife@gmail.com'
+license          'all_rights'
+description      'Installs/Configures chef-workstation'
+long_description 'Installs/Configures chef-workstation'
+version          '0.1.0'
+
+depends "python"
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: chef-workstation
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
